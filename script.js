64d82ee427c27e303a50643c72dfa554aacc28a8
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-05-23 13:30:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let output = document.getElementById('output');
+let button = document.getElementById('btn');
+let form = document.getElementById('form');
+
+async function display(event) {
+	let delay = parseInt(document.getElementById('delay').value);
+	let text = document.getElementById('text').value;
+	event.preventDefault();
+	setTimeout(() => {
+		console.log(text);
+		output.innerText = text;
+	},delay);
+}
+
+form.addEventListener('submit', display);
